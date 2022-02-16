3738821af831d3b629aa8083a5a4ca75d0f0e8e4
Ole Andreas <oarorvik@stud.ntnu.no>
2022-02-16 05:21:47 +0100

    Insane refactoring

--- library/graphics.py
+++ library/graphics.py
@@ -20,20 +20,47 @@ black = (0,0,0)
 
 selected = None
 
+worldImgSize = 40
+worldTileSideLength = 30
+worldTileWidth = worldTileSideLength/2 * 3**(1/2)
+
+#Coordinate translation
+def imgSize():
+    return worldImgSize*zoom
+
+def tileSideLength(): # = hypotenuses/tilesides
+    return worldTileSideLength*zoom 
+
+def tileWidth():
+    return tileSideLength()/2 * 3**(1/2)
+
+def screenToWorld(x, y):
+    return (
+        x/zoom + offsetX,
+        y/zoom + offsetY)
+
+def worldToScreen(x, y):
+    return (
+        (x - offsetX)*zoom,
+        (y - offsetY)*zoom)
+
+def indexToCoordinates(x, y):
+    return (
+        2*x*worldTileWidth + (y%2)*worldTileWidth,
+        1.5*y*worldTileSideLength)
+
+#Drawing
 def drawTileLine(win, startPosition, endPosition, color=black):
     pygame.draw.aaline(win, color, startPosition, endPosition) 
 
-tilesideLength = 30 # = hypotenuses/tilesides
-tileWidth = tilesideLength/2 * 3**(1/2)
-
 def drawTileOutline(win, x, y, color=black):
     #points
-    p1 = (x, y - tilesideLength*zoom)
-    p2 = (x + tileWidth*zoom, y-tilesideLength*zoom/2)
-    p3 = (x + tileWidth*zoom, y+tilesideLength*zoom/2)
-    p4 = (x, y + tilesideLength*zoom)
-    p5 = (x - tileWidth*zoom, y+tilesideLength*zoom/2)
-    p6 = (x - tileWidth*zoom, y-tilesideLength*zoom/2)
+    p1 = (x, y - tileSideLength())
+    p2 = (x + tileWidth(), y-tileSideLength()/2)
+    p3 = (x + tileWidth(), y+tileSideLength()/2)
+    p4 = (x, y + tileSideLength())
+    p5 = (x - tileWidth(), y+tileSideLength()/2)
+    p6 = (x - tileWidth(), y-tileSideLength()/2)
     
     drawTileLine(win, p1, p2, color)
     drawTileLine(win, p2, p3, color)
@@ -42,60 +69,32 @@ def drawTileOutline(win, x, y, color=black):
     drawTileLine(win, p5, p6, color)
     drawTileLine(win, p6, p1, color)
 
-def drawImgOnTile(win, x, y, img, size=40):
-    win.blit(pygame.transform.scale(img, (size*zoom, size*zoom)), (x-size*zoom/2,y-size*zoom/2))
+def drawImgOnTile(win, x, y, img, size):
+    win.blit(pygame.transform.scale(img, (size, size)), (x-size/2,y-size/2))
 
 def drawTiles(win, tileList):
     #Goes through tiles and renders them in aproprate coordinate on the screen.
     for y, row in enumerate(tileList):
         for x, tile in enumerate(row):
-            coordX, coordY = indexToCoordinates(x, y, offsetX, offsetY, zoom)
-            drawTileOutline(win, coordX, coordY)
+            worldX, worldY = indexToCoordinates(x, y)
+            screenX, screenY = worldToScreen(worldX, worldY)
+            drawTileOutline(win, screenX, screenY)
             if tile != " ":
                 img = pygame.image.load(os.path.join("assets", tile.img)).convert_alpha()
-                drawImgOnTile(win, coordX, coordY, img)
+                drawImgOnTile(win, screenX, screenY, img, imgSize())
 
-
-def indexToCoordinates(x, y, offsetX, offsetY, zoom):
-    coordY = 1.5*y*tilesideLength*zoom + offsetY
-    coordX = 2*x*tileWidth*zoom + tileWidth*zoom*(y%2) + offsetX
-    return (coordX, coordY)
-
-def distanceBetweenPoints(x1,y1,x2,y2):
-    return ((x1-x2)**2 + (y1-y2)**2)**0.5
-
-def coordinatesToIndex(x, y, offsetX, offsetY, zoom):
-    #The center that the point is closest to
-    currentClosest = None
-    ret = (None, None)
-    for indexY in range(len(tiles)):
-        for indexX in range(len(tiles[0])):
-            centerX, centerY = indexToCoordinates(indexX, indexY, offsetX, offsetY, zoom)
-            distance = distanceBetweenPoints(x,y,centerX,centerY)
-            if currentClosest == None or distance < currentClosest:
-                currentClosest = distance
-                ret = (indexX, indexY)
-    return ret
-
-def closestCenterPoint(x, y, offsetX, offsetY, zoom):
-    indexX, indexY = coordinatesToIndex(x, y, offsetX, offsetY, zoom)
-    return indexToCoordinates(indexX, indexY, offsetX, offsetY, zoom)
 
 def drawGame(win):
     win.fill(grassGreen) 
     drawTiles(win, tiles)
-    if selected != None:
-        x, y = indexToCoordinates(selected[0], selected[1], offsetX, offsetY, zoom)
-        drawTileOutline(win, x, y, tileSelectColor)
 
-
-
+#Change camera
 def drag(pos):
     global offsetX
     global offsetY
 
-    offsetX += pos[0]*0.5
-    offsetY += pos[1]*0.5
+    offsetX -= pos[0]*0.5
+    offsetY -= pos[1]*0.5
     
 
 def changeZoom(direction, pos): 
@@ -105,25 +104,9 @@ def changeZoom(direction, pos):
     newZoom = zoom + 0.15*direction *zoom
     if newZoom < 0.6: return
     elif newZoom > 3: return
-    else:
-        offsetX += (pos[0] - offsetX)/newZoom/2
-        offsetY += (pos[1] - offsetY)/newZoom/2
-        zoom = newZoom
-
+    zoom = newZoom
 
 def rightClick(win, pos):
     global selected
-    selected = coordinatesToIndex(pos[0], pos[1], offsetX, offsetY, zoom)
-    
 
-
-def screenToWorld((x, y)):
-    return (
-        x + offsetX,
-        y + offsetY)
-
-def worldToScreen(x,y,x,y):
-    return (
-        x - offsetX,
-        y - offsetY)
     

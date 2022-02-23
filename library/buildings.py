d7f513e28698ec19e2698fbb808b70ecd9d2c110
Ole Andreas <oarorvik@stud.ntnu.no>
2022-02-23 17:06:38 +0100

    House sprite

---
+++ library/buildings.py
@@ -0,0 +1,16 @@
+import pygame
+import os
+
+class House:
+    img = pygame.image.load(os.path.join("assets", "house.png"))
+    movementCost = 0
+    actionText = None
+
+    def endTurn(self):
+        return
+
+    def info(self):
+        "House"
+
+
+

7f4dd49be957394a69a7940dd8bdd93c7ef6706b
Ole Andreas <oarorvik@stud.ntnu.no>
2022-03-05 02:28:13 +0100

    building site (bad sprite)

--- library/buildings.py
+++ library/buildings.py
@@ -2,18 +2,21 @@ import pygame
 import os
 
 class House:
-    img = pygame.image.load(os.path.join("assets", "house.png"))
+    houseImg = pygame.image.load(os.path.join("assets", "house.png")) 
+    buildSiteImg = pygame.image.load(os.path.join("assets", "buildingsite.png"))
+    img = buildSiteImg
     movementCost = 0
     actionText = None
     buildButton = "houseButton"
     citizenIsSpawned = False
+    buildPointsLeft = 5
+    cost = {"wood":10} 
     
     def endTurn(self):
         if not self.citizenIsSpawned:
             from library.gamelogic import spawnCitizen
             spawnCitizen(self)
             self.citizenIsSpawned = True
-        return
 
     def isOccupied(self):
         return 
